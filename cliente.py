9cf2c962d4b9c976406be42ae9c0f22a72fea26e
Gustavo Adolfo Garcia <32319990+gustavogarciautp@users.noreply.github.com>
2018-10-27 20:24:58 -0500

    Incorporación de hilos y se agrega el modulo JSON

--- cliente.py
+++ cliente.py
@@ -1,20 +1,24 @@
 import socket
 import threading
+import json
 
 class Cliente():
     def __init__(self):
         self.s = socket.socket()
         self.s.connect(("localhost", 8000))
 
-        self.entrada()
+        #self.crearsala()
         
         thread_rcv= threading.Thread (target=self.receive)
         thread_rcv.daemon=True
         thread_rcv.start()
+
+        self.entrada()
         
         while True:
             output_data = input("> ")
-            self.s.send(username+": "+output_data)
+            cad=json.dumps({"Tipo":"Mensaje","msg":output_data})
+            self.s.send(cad.encode())
     
     def registrarse(self):
         username=input("Nombre: ")
@@ -23,28 +27,87 @@ class Cliente():
         password=input("Password: ")
         edad=input("Edad: ")
         genero=input("Genero: ")
-        cad="Registrarse "+username+" "+apellido+" "+login+" "+password+" "+edad+" "+genero
+        cad=json.dumps({"Tipo":"Registrarse","Nombre":username,"Apellido":apellido,"Login":login,
+        	"Password":password,"Edad":edad,"Genero":genero})
         self.s.send(cad.encode())
-        self.entrada()
+        #self.entrada()
 
     def startsesion(self):
     	username=input("Login: ")
     	password=input("Password: ")
-    	cad="startsession "+username+" "+password
+    	cad=json.dumps({"Tipo":"startsession","Login":username,"Password":password})
     	self.s.send(cad.encode())
-    	self.entrada()
+    	#self.entrada()
+
+    def crearsala(self):
+    	nombreSala=input("Nombre de la sala: ")
+    	cad=json.dumps({"Tipo":"#cR","Nombre":nombreSala})
+    	self.s.send(cad.encode())
+    	#self.entrada()
+
+    def entrarsala(self):
+    	nombreSala=input("Nombre de la sala: ")
+    	cad=json.dumps({"Tipo":"#gR","Nombre":nombreSala})
+    	self.s.send(cad.encode())
+
+    def salirsala(self):
+    	cad=json.dumps({"Tipo":"#eR"})
+    	self.s.send(cad.encode())
+
+    def msgprivado(self):
+    	r=input("Destinatario: ")
+    	msg=input("Mensaje: ")
+    	cad=json.dumps({"Tipo":"#\\Private","Receptor":r,"Mensaje":msg})
+    	self.s.send(cad.encode())
+
+    def exit(self):
+    	cad=json.dumps({"Tipo":"#exit"})
+    	self.s.send(cad.encode())
+
+    def showusers(self):
+    	cad=json.dumps({"Tipo":"#show users"})
+    	self.s.send(cad.encode())
+
+    def listarsalas(self):
+    	cad=json.dumps({"Tipo":"#lR"})
+    	self.s.send(cad.encode())
+
+    def eliminarsala(self):
+    	cad=json.dumps({"Tipo":"#dR"})
+    	self.s.send(cad.encode())
+
 
     def entrada(self):
-        opcion=input("1. Iniciar sesion\n2.Registrarse")
+        opcion=input("1. Iniciar sesion\n2.Registrarse\n3.Crear sala\n4.Entrar sala\n5.Salir sala\n6.Privado\n7.Desconectarse\n8.Listar salas\n9.Eliminar sala\n10.Mmostrar usuarios\n")
         if opcion=="1":
             self.startsesion()
-        else:
+        elif opcion=="2":
             self.registrarse()
+        elif opcion =="3":
+        	self.crearsala()
+        elif opcion =="4":
+        	self.entrarsala()
+        elif opcion=="5":
+        	self.salirsala()
+        elif opcion=="6":
+        	self.msgprivado()
+        elif opcion=="7":
+        	self.exit()
+        elif opcion=="8":
+        	self.listarsalas()
+        elif opcion=="9":
+        	self.eliminarsala()
+        elif opcion=="10":
+        	self.showusers()
+        opcion1=input("11.Mostrar nuevamente menu\n")
+        if opcion1=="11":
+        	self.entrada()
+
         
     def receive (self):
         while True:
             try:
-                data=self.s.recv(1024)
+                data=self.s.recv(1024).decode('utf-8')
                 if data:
                     print(data)
             except:
--- server.py
+++ server.py
@@ -1,86 +1,140 @@
 import socket
 import threading
+import json
 import pymongo
 import hashlib
 from pymongo import MongoClient
-#import pprint
+import pprint
 client = MongoClient()
 
 db = client.chatdistribuidos  #obtiene la base de datos
 
 usuarios = db.usuario
 salas = db.sala
-rooms=[]
+chatrooms={}
+users={}
 
 class usuario():
   	def __init__(self,conn,login,sala):
   		self.conexion=conn
   		self.login=login
   		self.sala=sala
-  		
-  	def crearsala(self,nombreSala):
-  		campo_nombre=salas.find_one({"Nombre":nombreSala})
-  		if campo_nombre:
-  			self.conexion.send("Sala ya existe".encode())
-  		else:
-  			campo = {"Nombre": nombreSala,
-  						"Creador":login,
-  						"Usuarios": [],
-  						}
-  			salas.insert_one(campo)
-  			sal=sala(nombreSala)
-  			salas.append(sal)
-  			self.sala=sal
-  			conexion.send("Se ha creado la sala".encode())
 
-  	def msgprivate(self,remitente,msg):
-  		self.conexion.send(msg)
+  		thread_send = threading.Thread(target=self.send)
+  		thread_send.daemon = True
+  		thread_send.start()
+
+  	def send(self):
+  		while True:
+  			try:
+  				data = json.loads(self.conexion.recv(1024).decode('utf-8'))
+  				if data["Tipo"]=="#cR":   #crear sala
+  					self.crearsala(data["Nombre"])
+  				elif data["Tipo"]=="#gR":   #Entrar a la sala
+  					self.entrarsala(data["Nombre"])
+  				elif data["Tipo"]=="#eR":
+  					self.salir()
+  				elif data["Tipo"]=="#\\Private": #Mensaje privado		
+  					self.msgprivate(data["Receptor"],data["Mensaje"])
+  				elif data["Tipo"]=="#exit":
+  					self.desconectar()
+  				elif data["Tipo"]=="#show users":
+  					self.show_users()
+  				elif data["Tipo"]=="Mensaje":
+  					data.update({"Emisor":self.login})
+  					self.sala.msg_to_all(data)
+  				elif data["Tipo"]=="#lR":
+  					self.listarsalas()
+  				else:# data["Tipo"]=="#dR":
+  					self.eliminarsala()
+  			except:
+  				pass
+			 
+
+  	def crearsala(self,nombreSala):
+  		chatroom=chatrooms.get(nombreSala,False) 
+  		if isinstance(chatroom,bool):
+  			chatroom=sala(self.login)
+  			chatrooms[nombreSala]=chatroom
+  			self.entrarsala(nombreSala)
+  			conexion.send("Se ha creado la sala".encode())
+  		else:
+  			self.conexion.send("Sala ya existe".encode())
+
+
+  	def entrarsala(self,nombreSala):
+  		self.sala.remove_users(self.login)
+  		chatroom=chatrooms[nombreSala]
+  		chatroom.add_users(self.login)
+  		self.sala=chatroom
+
+  	def salir(self):
+  		if self.sala.creador!="Server":
+  			self.sala.remove_users(self.login)
+  			self.sala=chatrooms["Default"]
+
+  	def desconectar(self):
+  		self.conexion.close()
+  		self.sala.remove(self)
+  		del users[self.login]
+
+  	def show_users(self):
+  		userslist=[]
+  		for usuario in usuarios.find():
+  			userslist.append(usuario['Login'])
+  		self.conexion.send(json.dumps(userslist).encode())
+
+  	def msgprivate(self,namereceptor,msg):
+  		recp=users[namereceptor]
+  		recp.conexion.send(json.dumps({"Tipo":"#\\Private","Emisor":self.login,"Mensaje":msg}).encode())
+
+  	def listarsalas(self):
+  		chatrooms_dict={}
+  		for name, chatroom in chatrooms.items():
+  			chatrooms_dict[name]=len(chatroom.clientes)
+  		self.conexion.send(json.dumps(chatrooms_dict).encode())
+
+  	def eliminarsala(self):
+  		if self.sala.creador==self.login:
+  			for cliente in self.sala.clientes:
+  				users[cliente].salir()
+  			chatrooms.remove(self.sala)
+  		else:
+  			self.conexion.send(json.dumps("Operacion no permitida").encode())
     
 class sala():
-	def __init__(self,nombre):
+	def __init__(self,creador):
 		self.clientes=[]
-		self.nombre=nombre
-		print("clientes: "+str(len(self.clientes)))
+		self.creador=creador
+		#print("clientes: "+str(len(self.clientes)))
 		#thread_send = threading.Thread(target=self.send)
-			
+		#self.add_users(self.creador)
 		#thread_send.daemon = True
 		#thread_send.start()
 
-	def msg_to_all(self, msg, conn):
-		print("name: " +self.nombre+" usuarios: "+str(len(self.clientes)))
-		for cliente in self.clientes:
-			if cliente.conexion != conn:
-				cliente.conexion.send(msg.encode())
+	def msg_to_all(self, data):
+		print(self.creador)
+		for receptor in self.clientes:
+			if receptor != data["Emisor"] and receptor!= "Server":
+				users[receptor].conexion.send(json.dumps(data).encode())
 
 	def add_users(self,cliente):
 		self.clientes.append(cliente)
-		#salas.update({"Nombre":self.nombre},{"$addToSet":{"Usuarios":usuario}})
 
-def show_users():
-  for usuario in usuarios.find():
-    print(usuario['Nombre'])
-
-def eliminarsala(nombre):
-  salas.remove({"Nombre":nombre})
-
-def show_salas():
-  for sala in salas.find():
-    print(sala['Nombre'],len(sala['Usuarios']))  #Salas + numero de usuarios
+	def remove_users(self,cliente):
+		self.clientes.remove(cliente)
 
 class Servidor():
 	"""docstring for Servidor"""
 	def __init__(self):
 
 		self.conexiones = []   
-		self.users=[]  #Usuarios logueados
-
 		self.s = socket.socket()
 		self.s.bind(("localhost", 8000))
-		self.s.listen(1)
+		self.s.listen(2)
 		self.s.setblocking(False)
-		default_sala=sala("Default")
-		rooms.append(default_sala)
-		print("rooms22: "+str(len(rooms)))
+
+		chatrooms['Default']=sala("Server")
 		thread_send = threading.Thread(target=self.send)
 		
 		thread_send.daemon = True
@@ -96,89 +150,40 @@ class Servidor():
 
 	def send(self):
 		while True:
-			if len(self.conexiones) > 0:
-				for c in self.conexiones:
-					try:
-						data = c.recv(1024).decode('utf-8')
-						if data.startswith("Registrarse"): 
-							data=data[12:]
-							data=data.split()
-							self.registrarse(data,c)
-						elif data.startswith("startsession"): 
-							data=data[13:]
-							data=data.split()
-							self.startsession(data,c)
-						elif data.startswith("#cR"):   #crear sala
-							nombreSala=data[5:-1]
-							user=self.find_users(c)
-							user.crearsala(nombreSala)
-						elif data.startswith("#gR"):   #Entrar a la sala
-							nombreSala=data[5:-1]
-							salap=self.find_salas(nombreSala)
-							user=self.find_users(c)
-							salap.add_users(user)
-						elif data.startswith("#\\"): #Mensaje privado
-							nombre_usuario=data[10:-1]
-							emisor=self.find_users(nombre_usuario)
-							receptor= self.find_users(c)
-							user.msgprivate(emisor,receptor)
-						elif data.startswith("<msg>"):
-							msg=data[6:]
-							user=self.find_users(c)
-							salap=user.sala
-							salap.msg_to_all(msg,c)
-						elif data.startswith("#lR"):
-							self.listarsalas()
-					except:
-						pass
+			for c in self.conexiones:
+				try:
+					data = json.loads(c.recv(1024).decode('utf-8'))
+					if data["Tipo"]=="Registrarse":
+						del data["Tipo"]
+						self.registrarse(data,c)
+					else: #data["Tipo"]=="startsession": 
+						del data["Tipo"]
+						self.startsession(data["Login"],data["Password"],c)
+				except:
+					pass
 
 	def registrarse(self,data,conexion):
-		campo_login=usuarios.find_one({"Login":data[2]})
+		campo_login=usuarios.find_one({"Login":data["Login"]})
 		if campo_login:
 			conexion.send("Login ya existe".encode())
 		else:
-			campo = {"Nombre": data[0],
-						"Apellido": data[1],
-						"Login": data[2],
-						"Password": data[3],#hashlib.sha1(data[3].encode()).hexdigest(),
-						"Edad": data[4],
-						"Genero": data[5]
-						}
-			usuarios.insert_one(campo)
-			conexion.send("Campo ingresado")
-			self.startsession(data[2],conexion)
+			data["Password"]=hashlib.sha1(data["Password"].encode()).hexdigest()
+			usuarios.insert_one(data)
+			conexion.send("Campo ingresado".encode())
+			self.startsession(data["Login"],data["Password"],conexion,True)
 
-	def startsession(self,data,conexion):
-		#global rooms
-		campo=usuarios.find_one({"Login":data[0],"Password":data[1]})#hashlib.sha1(data[1].encode()).hexdigest()})
-		if campo:
-			conexion.send("Sesion iniciada".encode())
-			user=usuario(conexion,data[0],rooms[0])
+	def startsession(self,username,password,conexion,flag=False):
+		campo=usuarios.find_one({"Login":username,"Password":hashlib.sha1(password.encode()).hexdigest()})
+		if campo or flag:
+			user=usuario(conexion,username,chatrooms['Default'])
 			#print("usuario creado")
-			self.users.append(user)
-			#self.conexiones.remove(conexion)
-			#print("rooms "+str(len(rooms)))
-			rooms[0].add_users(user)
+			users[username]=user
+			self.conexiones.remove(conexion)
+			#print("rooms "+str(len(chatrooms)))
+			chatrooms['Default'].add_users(username)
+			conexion.send("Sesion iniciada".encode())
 		else:
 			conexion.send("Datos incorrectos".encode())
-
-	def find_users(self,u):
-		for user in self.users:
-			if isinstance(u,str):
-				if user.login==u:
-					return user
-			else:
-				print("conn "+str(user.conexion))
-				if user.conexion==u:
-					return user
-
-	def find_salas(self,nameSala):
-		for salap in salas:
-			if salap.nombre==nameSala:
-				return salap
-
-	def listarsalas(self):
-		for s in salas.find():
 
 
 s = Servidor()
